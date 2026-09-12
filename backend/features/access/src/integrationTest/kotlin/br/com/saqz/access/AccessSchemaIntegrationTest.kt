4335ca9a1fcf74073f58cca25e2094c10e2c4ad0
Bruno Henrique Almeida <bruno_almeida@Brunos-MacBook-Pro-2.local>
2026-09-12 12:58:36 -0300

    test(access): match exact migration inventory

--- backend/features/access/src/integrationTest/kotlin/br/com/saqz/access/AccessSchemaIntegrationTest.kt
+++ backend/features/access/src/integrationTest/kotlin/br/com/saqz/access/AccessSchemaIntegrationTest.kt
@@ -25,7 +25,7 @@ class AccessSchemaIntegrationTest {
             .dataSource(database.jdbcUrl, database.username, database.password)
             .locations("classpath:db/migration")
             .load()
-        assertEquals(13, flyway.migrate().migrationsExecuted)
+        assertEquals(14, flyway.migrate().migrationsExecuted)
     }
 
     @BeforeEach
